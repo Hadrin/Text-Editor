0804706c4251c752806963eb341f8d34f5521291
David McGinley <dmstar9@gmail.com>
2022-05-11 22:07:24 -0400

    Fixed PWA install by adding 144px option to icon

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,16 +3,19 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('PWA not installed');
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
+    console.log('PWA not installed');
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
+    if(!promptEvent){
+        return;
+    }
     promptEvent.prompt();
-    window.deferredPrompt = null;
+    // window.deferredPrompt = null;
     butInstall.classList.toggle('hidden', true);
 });
 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,50 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        name: "Text Editor",
+        short_name: "JATE",
+        description: "A Text Editor with a Service Worker",
+        background_color: "#A0A0A0",
+        crossorigin: "anonymous",
+        fingerprints: false,
+        inject: true,
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [92, 128, 144],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', ['@babel/transform-runtime', { helpers: false, regenerator: true }]],
+            }
+          }
+        }
       ],
     },
   };
