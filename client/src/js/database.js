a00caad70152a18378909d8e8f4b50a995ef42b3
David McGinley <dmstar9@gmail.com>
2022-05-11 20:47:41 -0400

    Power is flickering, pushing just in case

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const trans = db.transaction('jate', 'readwrite');
+  const store = trans.objectStore('jate');
+  const req = store.put({ id: 1, value: content});
+  const result = await req;
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const trans = db.transaction('jate', 'readonly');
+  const store = trans.objectStore('jate');
+  const req = store.get(1);
+  const result = await req;
+  return result.value;
+}
 
 initdb();
